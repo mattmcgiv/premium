d1ba3247be874c791bced445816721e6fbc8d5ea
Matt McGivney <matt@antym.com>
2014-08-08 08:26:25 -0400

    Updated license, copyright info, & changed JS namespace.

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -14,4 +14,17 @@
 /Users/matthewmcgivney/Code/premium/platforms/android/res/drawable-xhdpi/icon.png \
 /Users/matthewmcgivney/Code/premium/platforms/android/res/values/strings.xml \
 /Users/matthewmcgivney/Code/premium/platforms/android/res/xml/config.xml \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable/icon.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-hdpi/icon.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-land-hdpi/screen.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-land-ldpi/screen.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-land-mdpi/screen.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-land-xhdpi/screen.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-ldpi/icon.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-mdpi/icon.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-port-hdpi/screen.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-port-ldpi/screen.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-port-mdpi/screen.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-port-xhdpi/screen.png \
+/Users/matthewmcgivney/Code/premium/platforms/android/ant-build/res/drawable-xhdpi/icon.png \
 /Users/matthewmcgivney/Code/premium/platforms/android/ant-build/AndroidManifest.xml \
